ff94a1810c1b6ffc4bd04ea855d9d420e69e5d8e
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-09-27 21:49:57 -0300

    a

--- src/components/Contacto/Formulario.jsx
+++ src/components/Contacto/Formulario.jsx
@@ -1,45 +1,6 @@
 import React from "react";
-import { useForm } from "./useForm";
-
-const initialForm = {
-  name:"",
-  email:"",
-  subject:"",
-  comment:"",
-};
-
-const validationsForm = (form) => {
-  let errors = {};
-  let regexName =  /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
-   let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-   let regexComment = /^.{1,255}$/;
-
-  if (!form.name.trim()) {
-    errors.name = "el campo Nombre es requerido"
-  } else if(!regexName.test(form.name.trim())) {
-    errors.name = 'El campo nombre solo acepta letras y espacios vacios'
-  }
-
-  if (!form.email.trim()) {
-    errors.email = "el campo email es requerido"
-  } else if(!regexEmail.test(form.email.trim())) {
-    errors.email = 'El campo email es incorrecto'
-  }
-
-  if (!form.comment.trim()) {
-    errors.comment = "el campo comment es requerido"
-  }
-
-  if (!form.subject.trim()) {
-    errors.subject = "el campo subject es requerido"
-  } else if(!regexComment.test(form.comment.trim())) {
-    errors.comment = 'El campo acepta hasta 255 caracteres'
-  }
 
 
-
-  return errors;
-};
 
 let styles = {
   fontWeight:"bold",
@@ -47,15 +8,7 @@ let styles = {
 }
 
 export const Formulario = () => {
-  const {
-    form,
-    errors,
-    loading,
-    response,
-    handleChange,
-    handleBlur,
-    handleSubmit,
-  } = useForm(initialForm, validationsForm);
+  
   return (
     <div>
       <form name="contact" method="POST" data-netlify="true">
