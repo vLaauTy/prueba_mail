cec2f6cb5866894a5ed9813e1148642550c13a7b
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-09-27 22:12:48 -0300

    rest form clasico

--- src/components/Contacto/Formulario.jsx
+++ src/components/Contacto/Formulario.jsx
@@ -1,6 +1,45 @@
 import React from "react";
+import { useForm } from "./useForm";
+
+const initialForm = {
+  name:"",
+  email:"",
+  subject:"",
+  comment:"",
+};
+
+const validationsForm = (form) => {
+  let errors = {};
+  let regexName =  /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
+   let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+   let regexComment = /^.{1,255}$/;
+
+  if (!form.name.trim()) {
+    errors.name = "el campo Nombre es requerido"
+  } else if(!regexName.test(form.name.trim())) {
+    errors.name = 'El campo nombre solo acepta letras y espacios vacios'
+  }
+
+  if (!form.email.trim()) {
+    errors.email = "el campo email es requerido"
+  } else if(!regexEmail.test(form.email.trim())) {
+    errors.email = 'El campo email es incorrecto'
+  }
+
+  if (!form.comment.trim()) {
+    errors.comment = "el campo comment es requerido"
+  }
+
+  if (!form.subject.trim()) {
+    errors.subject = "el campo subject es requerido"
+  } else if(!regexComment.test(form.comment.trim())) {
+    errors.comment = 'El campo acepta hasta 255 caracteres'
+  }
 
 
+
+  return errors;
+};
 
 let styles = {
   fontWeight:"bold",
@@ -8,28 +47,31 @@ let styles = {
 }
 
 export const Formulario = () => {
-  
+  const {
+    form,
+    errors,
+    loading,
+    response,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+  } = useForm(initialForm, validationsForm);
   return (
-    <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-
+    <div>
+      <h2>Formulario</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="nombre" onChange={handleChange} onBlur={handleBlur} value={form.name} required />
+        {errors.name && <small style={styles}>{errors.name}</small>}
+        <input type="email" name="email" placeholder="email" onChange={handleChange} onBlur={handleBlur} value={form.email} required />
+        {errors.email && <small style={styles}>{errors.email}</small>}
+        <input type="text" name="subject" placeholder="asunto" onChange={handleChange} onBlur={handleBlur} value={form.subject} required />
+        {errors.subject && <small style={styles}>{errors.subject}</small>}
+        <textarea name="comment"  cols="50" rows="5" placeholder="escribe" onChange={handleChange} onBlur={handleBlur} value={form.comment} required></textarea>
+        {errors.comment && <small style={styles}>{errors.comment}</small>}
+        <input type="submit" value="ENVIAR" />
+      </form>
+      {loading && <p>Cargando...</p>}
+      {response && <h3>Mensaje enviado con exito!</h3>}
+    </div>
   );
 };
